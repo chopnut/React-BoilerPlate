a84f44466d72efad07f639ff196b381a270aa296
Ernani Danting <bprizze@yahoo.com>
2018-06-21 17:59:23 +1000

    Modifiying boilerplate

--- front-end/src/react/Layout.js
+++ front-end/src/react/Layout.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Layout extends Component {
   render() {
-    return <div>Hello world up</div>;
+    return <div>Hello world up!</div>;
   }
 }
 
---
+++ push.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Modifiying boilerplate"
+git push origin master
